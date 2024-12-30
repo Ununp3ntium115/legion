bc104d2cc948da983c166017ff24dce12bcbd200
Ununp3ntium115 <126101872+Ununp3ntium115@users.noreply.github.com>
2024-12-30 14:41:30 -0700

    Update runIt.sh

--- docker/runIt.sh
+++ docker/runIt.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
-docker pull gvit/legion
+# Pull the latest Legion Docker image
+# Uncomment the following line if using the gvit image
+# docker pull gvit/legion
+
+echo "Starting Legion with Docker..."
 
 if [[ ! -z $1 ]]
 then
     export DISPLAY=$1:0.0
     XSOCK=/tmp/.X11-unix
     XAUTH=/tmp/.docker.xauth
-    rm /tmp/.docker.xauth* -f
+    rm -f $XAUTH
     touch $XAUTH
-    xauth add $DISPLAY - `mcookie`
+    xauth add $DISPLAY . `mcookie`
     xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-    docker run -ti -v $XSOCK -v $XAUTH -e XAUTHORITY=$XAUTH -e DISPLAY=$DISPLAY gvit/legion
+    docker run -ti \
+        -v $XSOCK:/tmp/.X11-unix \
+        -v $XAUTH:/tmp/.docker.xauth \
+        -e XAUTHORITY=$XAUTH \
+        -e DISPLAY=$DISPLAY \
+        --net=host \
+        --security-opt apparmor:unconfined \
+        --security-opt label:disable \
+        legion:latest
 else
-    docker run -ti -e DISPLAY=$DISPLAY --net=host --security-opt=apparmor:unconfined --security-opt=label:disable gvit/legion
+    docker run -ti \
+        -e DISPLAY=$DISPLAY \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        --net=host \
+        --security-opt apparmor:unconfined \
+        --security-opt label:disable \
+        legion:latest
 fi
